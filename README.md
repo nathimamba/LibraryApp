cc15b29fcdc3ec86e2a40bfd2c08c43233646ed1
Nkosinathi <125558007+nathimamba@users.noreply.github.com>
2023-05-19 18:09:28 +0200

    Adding of new files

---
+++ README.md
@@ -0,0 +1,23 @@
+# The Odin Project Web Development 
+## Project: LibraryApp
+
+SignUp Form is a project from The Odin Project [curriculum](https://www.theodinproject.com/lessons/node-path-javascript-library).
+
+**Live preview** of my project [here](https://nathimamba.github.io/LibraryApp/)
+
+### Tools: ###
+* Bootstrap 5
+
+### Outcome of this Projec: ###
+* Learning to write objects in JavaScript
+* Organising code using JavaScript Objects
+* Store objects in Array and Access them
+
+### Requirements: ###
+* A render() function that loops through the array and displays each book on the page
+* A form that allows user to add new books
+* A delete button.
+* A button to handle the read status of the book
+
+### Updates: ###
+N.B ~ The program will be updated to use BaaS(Backend as a Service) at a later stage or a local storage using JSON files to store objects so history can be stored.
---
+++ js/main.js
@@ -0,0 +1,96 @@
+//library Array 
+
+const DEFAULT_DATA = [
+    { title: "The Lord of the Rings", author: "Tolkien", pages: 900, status: "READ" },
+    {
+        title: "Alice in Wonderland",
+        author: "Lewis Caroll",
+        pages: 750,
+        status: "NOT READ",
+    },
+    { title: "Naruto", author: "Masashi Kishimoto", pages: 1500, status: "READ" },
+];
+
+let myLibrary = [];
+
+myLibrary = DEFAULT_DATA;
+
+//the constructor object
+
+function Books(title, author, pages, status) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+}
+
+
+function addBookToLibrary() {
+
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+    const statusCheckbox = document.querySelector('#status');
+
+    let status = statusCheckbox.checked ? 'READ' : 'NOT READ';
+    let book = new Books(title, author, pages, status);
+    myLibrary.push(book);
+}
+
+const tableBody = document.querySelector('#bookTable');
+
+function displayBooks() {
+
+
+    tableBody.innerHTML = '';
+    let count = 1;
+
+    myLibrary.forEach(book => {
+        const htmlBook = `<tr>
+        <td>${count++}</td>
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+         <td>${book.pages}</td>
+        <td><button class="status-button">${book.status}</button></td>
+        <td><button class="delete">DELETE</button></td>
+        </tr>`;
+        tableBody.insertAdjacentHTML("beforeend", htmlBook);
+    });
+}
+
+
+//-----------------------------------------------------------------------------------------
+
+displayBooks();
+
+const form = document.querySelector('form');
+const modal = document.querySelector(".modal");
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addBookToLibrary();
+    displayBooks();
+    modal.style.display = "none";
+    form.reset();
+})
+
+tableBody.addEventListener('click', (e) => {
+    if (e.target.classList.contains('status-button')) {
+        const index = e.target.parentNode.parentNode.rowIndex - 1;
+        if (myLibrary[index].status === 'read') {
+            myLibrary[index].status = 'not read';
+        } else {
+            myLibrary[index].status = 'read';
+        }
+        displayBooks();
+    }
+});
+
+
+tableBody.addEventListener('click', (e) => {
+    if (e.target.classList.contains('delete')) {
+        const index = e.target.parentNode.parentNode.rowIndex - 1;
+        myLibrary.splice(index, 1);
+        displayBooks();
+    }
+});
